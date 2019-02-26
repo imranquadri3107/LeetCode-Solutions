58494b83a45fc44b0e6321b3c0dae0adda17ce54
imranquadri3107 <imranquadri77@gmail.com>
2019-02-25 17:06:15 -0800

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,23 @@
+// Solution 2
+
+function twoSum(nums, target){
+
+  for(var i=0; i<nums.length; i++){
+    for(var j = i+1; j<nums.length; j++){
+        if(nums[i]+ nums[j]===target){
+          return [i,j];
+        }
+    }
+  } return false;
+}
+twoSum([2,7,11,15], 22);
+
+// Output: 1,3
+
+
+
+
+// Solution 2
 var twoSum = function(nums, target) {
     var ret = [];
     var exist = {};
